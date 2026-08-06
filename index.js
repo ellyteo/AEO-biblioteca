779b7031c7a7d7e7fffa03da044724691b35ff41
Gustavo Cabral <gustavosilva010304@gmail.com>
2026-08-06 19:35:24 -0300

    ja terminei

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import livro from './router-Livros/livros.js'
+import emprestimo from './router-Livros/emprestimos.js'
+
 
 const app = express()
 app.use(express.json())
 
 app.use('/api/v1/livro', livro)
-
+app.use('/api/v1/emprestimo',emprestimo)
 app.listen(3000, () => {
     console.log("Servidor escutando na porta 3000")
 })
