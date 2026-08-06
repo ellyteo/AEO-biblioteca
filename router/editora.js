4a4376a97d63258f17995f1c7c3c2489f47a2cad
Nathalia <nathimartins590@gmail.com>
2026-08-06 18:50:06 -0300

    Nathalia-editora

--- router/editora.js
+++ router/editora.js
@@ -1,42 +1,60 @@
 import express from 'express'
-import editora from './repository/editora.js'
+import editora from '../repository/editora'
+
 
 const router = express.Router()
 
-router.get("/api/editoras", (req, res) => {
-    res.send({ pessoas: editora })
+router.get("/", (req, res) => {
+    res.send({ editora : editoras })
 })
 
-router.get("/api/editoras/:id", (req, res) => {
-    const id = req.params.id
-    const editoras = editora.find(it => it.id == id)
-    if (!editoras) {
-        res.send({ message: "Editora não encontrada" })
-        return
-    }
-    res.send({ editoras })
-})
-
-router.get("/api/editoras2", (req, res) => {
+router.get("/registrar", (req, res) => {
     const { id, nome } = req.query
     if(!id || !nome) {
         res.send({ message: "Favor informar id e nome da editora" })
         return 
     }
     editora.push({ id, nome })
+    console.log(editora)
+    
     res.send({ message: "Editora adicionada com sucesso" })
 })
 
-router.get("/api/alterarEditora/:id", (req, res) => {
+router.get("/:id", (req, res) => {
+    const id = req.params.id
+
+    const editoras = editora.find(it => it.id == id)
+
+    if (!editoras) {
+        res.send({ message: "Editora não encontrada" })
+        return
+    }
+
+    res.send({ editora })
+})
+
+router.get("/alterar/:id", (req, res) => {
     const id = req.params.id
     const { nome } = req.query
     const editoras = editora.find(it => it.id == id)
+
     if(!editoras) {
-        res.send({ message: "Favor informar id" })
+        res.send({ message: "Favor informar o id" })
         return 
     }
     editora.nome = nome
     res.send({ message: "Editora alterada com sucesso" })
+})
+
+router.get("/deletar/:id", (req, res) => {
+    const id = req.params.id
+    const editoras = editora.find(it => it.id == id)
+    if(!editoras) {
+        res.send({ message: "Favor informar id e name" })
+        return 
+    }
+    editora.splice(it => it.id == id, 1)
+    res.send({ message: "Editora deletada com sucesso" })
 })
 
 export default router
